809e1e2c16c16124b112c19746445ec48a458eb6
Momo2015-AI <148732234+Momo2015-AI@users.noreply.github.com>
2026-08-07 00:12:42 +0800

    LAB1.0: add experiment learning model

---
+++ content/experiments/experiment-schema.js
@@ -0,0 +1,12 @@
+// ChemLab-G9 LAB1.0 experiment model
+export const Experiment = {
+  id: '',
+  title: '',
+  question: '',
+  materials: [],
+  steps: [],
+  observation: '',
+  conclusion: '',
+  errorAnalysis: [],
+  examTransfer: ''
+};
